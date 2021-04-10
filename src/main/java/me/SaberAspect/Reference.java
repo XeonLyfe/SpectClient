3c6298d58657c822ba03636bc4004a98e68b34a1
SaberAspect <74943836+SaberAspect@users.noreply.github.com>
2021-04-09 20:55:16 -0400

    Add files via upload

---
+++ src/main/java/me/SaberAspect/Reference.java
@@ -0,0 +1,10 @@
+package me.SaberAspect;
+
+public class Reference {
+	public static final String MOD_ID = "spc";
+	public static final String NAME = "SpectClient";
+	public static final String VERSION = "0.5.2";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "me.SaberAspect.SpectClient.api.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "me.SaberAspect.SpectClient.api.proxy.CommonProxy";
+}
